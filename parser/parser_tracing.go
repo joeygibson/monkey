916c2b70793352fba6b57359f73e44231ce0e857
Joey Gibson <joegibso@cisco.com>
2017-07-13 07:46:33 -0400

    Made tracing depend on env var

--- parser/parser_tracing.go
+++ parser/parser_tracing.go
@@ -2,12 +2,18 @@ package parser
 
 import (
 	"fmt"
+	"os"
 	"strings"
 )
 
 var traceLevel int = 0
 
 const traceIdentPlaceholder string = "\t"
+
+func isEnvVarSet() bool {
+	_, ok := os.LookupEnv("TRACE")
+	return ok
+}
 
 func identLevel() string {
 	return strings.Repeat(traceIdentPlaceholder, traceLevel-1)
@@ -21,12 +27,17 @@ func incIdent() { traceLevel = traceLevel + 1 }
 func decIdent() { traceLevel = traceLevel - 1 }
 
 func trace(msg string) string {
-	incIdent()
-	tracePrint("BEGIN " + msg)
+	if isEnvVarSet() {
+		incIdent()
+		tracePrint("BEGIN " + msg)
+	}
+
 	return msg
 }
 
 func untrace(msg string) {
-	tracePrint("END " + msg)
-	decIdent()
+	if isEnvVarSet() {
+		tracePrint("END " + msg)
+		decIdent()
+	}
 }
