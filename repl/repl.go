f2974293a5ed02e864ddc4850f498a234a14c2c4
Joey Gibson <joey@joeygibson.com>
2017-07-15 23:19:42 -0400

    rEpl

--- repl/repl.go
+++ repl/repl.go
@@ -3,6 +3,7 @@ package repl
 import (
 	"bufio"
 	"fmt"
+	"github.com/joeygibson/monkey/evaluator"
 	"github.com/joeygibson/monkey/lexer"
 	"github.com/joeygibson/monkey/parser"
 	"io"
@@ -32,8 +33,11 @@ func Start(in io.Reader, out io.Writer) {
 			continue
 		}
 
-		io.WriteString(out, program.String())
-		io.WriteString(out, "\n")
+		evaluated := evaluator.Eval(program)
+		if evaluated != nil {
+			io.WriteString(out, evaluated.Inspect())
+			io.WriteString(out, "\n")
+		}
 	}
 }
 
